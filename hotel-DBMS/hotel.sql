3c2cdb28aa6a619cf39aafd065ee98d1677d42fd
Kali Live user <getachewkibr199@gmail.com>
2025-03-29 17:19:22 +0000

    additional table created

--- hotel-DBMS/hotel.sql
+++ hotel-DBMS/hotel.sql
@@ -1,3 +1,5 @@
+
+
 CREATE table IF NOT EXISTS guest(
     guestId int PRIMARY KEY AUTO_INCREMENT,
     firstName VARCHAR(40),
@@ -47,6 +49,70 @@ create table payment (
     paymentStatus ENUM('completed', 'failed', 'refunded') DEFAULT 'completed',
     Foreign Key (reservationsId) REFERENCES reservation (reservationid)
 )
+
+create table staff (
+    staffId int PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) ,
+    position VARCHAR(50) NOT NULL ,
+    phoneNumber VARCHAR(60),
+    hireDate DATE,
+    salary DECIMAL(5,2),
+    _status ENUM('active', 'on leave', 'terminated' ) DEFAULT 'active'
+)
+
+
+create TABLE services(
+    serviceId int PRIMARY KEY AUTO_INCREMENT,
+    serviceName VARCHAR(100) NOT NULL,
+    price DECIMAL(6,2),
+    availablity ENUM('24/7', 'daytime', 'on-request')
+)
+
+
+create TABLE guestServices(
+    guestServiceId int PRIMARY key AUTO_INCREMENT,
+    reservation_Id int NOT NULL,
+    service_id int NOT NULL,
+    requestDate DATE DEFAULT CURRENT_TIMESTAMP,
+    quantity int DEFAULT 1,
+    guestServiceStatus ENUM('requested', 'in-progress', 'completed', 'cancelled'),
+    totatlCharge DECIMAL(10,2),
+    Foreign Key (reservation_Id) REFERENCES reservation(reservationid),
+    Foreign Key (service_id) REFERENCES services(serviceId)
+)
+
+
+
+create table inventory(
+    inventoryId int PRIMARY key  AUTO_INCREMENT,
+    intemName VARCHAR(100) NOT NULL ,
+    quantiyInStock int ,
+    unitOfMeasure VARCHAR(10),
+    suplierInfo TEXT 
+)
+ -----not finish bellow 
+create table Maintenance(
+  maintenance_id int PRIMARY KEY AUTO_INCREMENT,
+  room_id int ,
+  staffId int NOT NULL ,
+  issueType VARCHAR(100) NOT NULL,
+  reportDate date ,
+
+)
+
+
+--      10. Maintenance Table       ////
+-- - maintenance_id (PK, INT, AUTO_INCREMENT)
+-- - room_id (FK, INT, NOT NULL)
+-- - staff_id (FK, INT, NOT NULL)
+-- - issue_type (VARCHAR(100), NOT NULL) (e.g., "Plumbing", "Electrical")
+-- - description (TEXT)
+-- - report_date (DATETIME, DEFAULT CURRENT_TIMESTAMP)
+-- - start_date (DATETIME)
+-- - completion_date (DATETIME)
+-- - status (ENUM('reported', 'in-progress', 'completed'))
+-- - cost (DECIMAL(10,2))
 
 
 
